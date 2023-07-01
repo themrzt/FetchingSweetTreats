56ddf987e32059e4fd1107b5386353a08c347ef6
themrzt <themrzt@gmail.com>
2023-07-01 18:51:17 -0500

    Initial Commit

---
+++ SweetTreats/SweetTreatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetTreatsApp.swift
+//  SweetTreats
+//
+//  Created by Zach Tarvin on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SweetTreatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
